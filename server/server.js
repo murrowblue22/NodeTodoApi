d2c452eeb0d9b7b7582c6009edfc2fef9b1f99f4
James Pinkney <murrowblue24@yahoo.com>
2019-03-02 14:21:18 -0500

    Add Patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express'); 
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -50,6 +51,39 @@ app.get('/todos/:id', (req, res) => {
     });
 }); 
 
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null; 
+    }
+
+    Todo.findOneAndUpdate(
+        {_id: id}, 
+        {$set: body},
+        {new: true}
+    )
+    .then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    })
+    .catch((e) => {
+        res.status(400).send();
+    })
+});
 
 app.delete('/todos/:id', (req, res) => {
     // get the id 
