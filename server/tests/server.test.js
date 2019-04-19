b8ca7115db7406a1bfb68d87a0a9205b4156089b
James Pinkney <murrowblue24@yahoo.com>
2019-04-19 18:41:18 +0000

    Add tsts for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -224,7 +224,8 @@ describe('POST /users', () => {
                     expect(user).toBeDefined(); 
                     expect(user.password).not.toBe(password);
                     done();
-                });
+                })
+                .catch((e) => done(e)); 
             });
     }); 
     
@@ -252,5 +253,52 @@ describe('POST /users', () => {
             .expect(400)
             .end(done);
     }); 
+    
+});
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        request(app)
+        .post('/users/login')
+        .send({
+            email: users[1].email,
+            password: users[1].password
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toBeDefined();
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+            
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens[0]).toHaveProperty(
+                    "token", res.headers['x-auth']
+                );
+                expect(user.tokens[0]).toHaveProperty(
+                    "access", "auth"
+                );
+                done(); 
+            })
+            .catch((e) => done(e)); 
+        });
+    });
+    
+    it('should reject invalid login', (done) => {
+        request(app)
+        .post('/users/login')
+        .send({
+            email: users[1].email,
+            password: "wrongPassword"
+        })
+        .expect(400)
+        .expect((res) => {
+            expect(res.headers['x-auth']).not.toBeDefined();
+        })
+        .end(done);
+    });
+    
     
 });
