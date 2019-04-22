03b43390cc71db4fdacbb75f641613999f9f5cc1
James Pinkney <murrowblue24@yahoo.com>
2019-04-22 05:18:58 +0000

    added expect upgraded method call

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -327,12 +327,10 @@ describe('POST /users/login', () => {
             }
             
             User.findById(users[1]._id).then((user) => {
-                expect(user.tokens[1]).toHaveProperty(
-                    "token", res.headers['x-auth']
-                );
-                expect(user.tokens[1]).toHaveProperty(
-                    "access", "auth"
-                );
+                expect(user.toObject().tokens[1]).toMatchObject({
+                    access: 'auth',
+                    token: res.headers['x-auth']
+                });
                 done(); 
             })
             .catch((e) => done(e)); 
